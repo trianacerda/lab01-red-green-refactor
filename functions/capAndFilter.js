42b5906d78c263c41bc6361f4f2312a95836e12c
Triana Cerda <trianaRcerda@gmail.com>
2021-09-13 16:50:30 -0700

    red test for capAndFilter + refactored test

--- functions/capAndFilter.js
+++ functions/capAndFilter.js
@@ -1,2 +1,6 @@
-const capitalizeAndFilter = (arr) => {};
+const capitalizeAndFilter = (arr) => {
+  for (const string of arr) !string.startsWith('f' || 'F');
+  return arr;
+};
+
 export default capitalizeAndFilter;
--- tests/capAndFilter.test.js
+++ tests/capAndFilter.test.js
@@ -1,9 +1,8 @@
 import capitalizeAndFilter from '../functions/capAndFilter.js';
 
 describe('capitalizeAndFilter', () => {
-  it(
-    'takes an array of strings--capitalize all strings and filter out any string that starts with the letter F/f.'
-  );
-  const randomArray = ['Frank', 'the', 'Ford', 'is', 'fun'];
-  expect(capitalizeAndFilter(randomArray)).toEqual('THE', 'IS');
+  it('takes arr and caps + filter it without "F/f"', () => {
+    const randomArray = ['Frank', 'the', 'Ford', 'is', 'fun'];
+    expect(capitalizeAndFilter(randomArray)).toEqual(['THE', 'IS']);
+  });
 });
