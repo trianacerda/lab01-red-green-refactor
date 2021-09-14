5aed3467092c7e161de8c10536a4e4ebf97808eb
Triana Cerda <trianaRcerda@gmail.com>
2021-09-13 20:02:12 -0700

    refactored getQuotes --URL

--- functions/fetchQuotes.js
+++ functions/fetchQuotes.js
@@ -1,8 +1,7 @@
 import fetch from 'node-fetch';
 
 const fetchQuotes = async () => {
-  const URL = 'https://futuramaapi.herokuapp.com/api/quotes';
-  const response = await fetch(URL);
+  const response = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
   const data = await response.json();
   const formattedData = {
     name: data[0].character,
