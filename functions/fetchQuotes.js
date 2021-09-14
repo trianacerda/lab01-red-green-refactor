858c1ccef9414954c6527a09fb02fee561707ee7
Triana Cerda <trianaRcerda@gmail.com>
2021-09-13 18:55:55 -0700

    pausing points. to be continued on fetchQuotes

--- functions/fetchQuotes.js
+++ functions/fetchQuotes.js
@@ -1,4 +1,13 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-const fetchQuotes = async () => {};
-export default fetchQuotes;
+// const fetchQuotes = async () => {
+//   const URL = 'https://futuramaapi.herokuapp.com/api/characters/bender';
+//   const response = await fetch(URL);
+//   const data = response.json();
+
+//   await console.log('!!!!!!!', data);
+
+//   return data[0].map((character) => character.quote);
+// };
+
+// export default fetchQuotes;
